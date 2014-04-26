5a95141036c66ab38bcd92f666f8f5a76d0884d9
Kinnnnn <kin.tang@gmail.com>
2014-04-26 23:40:11 +0800

    added overall monty carlo %

--- kinkincup/kinkincup.js
+++ kinkincup/kinkincup.js
@@ -231,7 +231,7 @@ App.MontyCarlo = Backbone.Model.extend({
         var draw = 0;
         var lose = 0;
 
-        _(200).times(function(n) {
+        _(100).times(function(n) {
             var randomDeck = _.shuffle(deck);
             var hc = heroCards;
             var vc = villianCards || [randomDeck.shift(), randomDeck.shift()];
