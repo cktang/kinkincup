dae7ae484154e13bb651d844a443df27402796ab
Kinnnnn <kin.tang@gmail.com>
2014-07-01 18:51:39 +0800

    activityDetails done

--- silly.html/backbonemodel.js
+++ silly.html/backbonemodel.js
@@ -23,31 +23,48 @@ Handlebars.registerHelper('filter', function(list, options) {
   return new Handlebars.SafeString(_(list).where(options.hash).length);
 });
 
+Handlebars.registerHelper('printStatus', function(object) {
+  return new Handlebars.SafeString(object.status == "going"? "success": object.status == "notgoing"? "danger" : "default" );
+});
+
 App.Mixins.HandlebarModelRenderer = {
 	render: function() {
-		if (!this.el || !this.collection) return;
+		if (!this.el || (!this.collection && !this.model)) return;
 		var self = this;
 
 		//clear screen
 		$(this.el).find('*').not('script.template').empty().remove();
 
-		this.collection.each(function(model, i) {
-			self.renderModel(self.el, model)
-		});
+		if (this.collection) {
+			this.collection.each(function(model, i) {
+				self.renderModel(self.el, model)
+			});
+		}
+
+		if (this.model) this.renderModel(this.el, this.model);
 
 		this.trigger('rendered');
 	},
 
 	renderModel: function(container, model) {
-		var el = this.realRenderModel(container, model);
+		var existingEl = $(container).find('[data-id=' + model.id + ']')[0];
+		var el = this.realRenderModel(container, model, existingEl);
+		$(el).find('.hide').removeClass('hide').hide();
 		this.registerListeners(el);	
 		this.handleEmpty();
 	},
 	
-	realRenderModel: function(container, model) {
+	realRenderModel: function(container, model, el) {
 		if ($(container).find('script.template').length == 0) return;
 		this.template = this.template || Handlebars.compile($(container).find('script.template').html());
-		return this.template && $(this.template(model.toJSON())).appendTo($(container)).hide().show('fade');
+
+		if (el && this.template) {
+			var newEl = $(this.template(model.toJSON())).insertAfter($(el)).hide().show('fade');
+			$(el).remove();
+			return newEl;
+		} else {
+			return this.template && $(this.template(model.toJSON())).appendTo($(container)).hide().show('fade');
+		}
 	},
 
 	removeModel: function(container, model) {
@@ -57,7 +74,7 @@ App.Mixins.HandlebarModelRenderer = {
 
 	handleEmpty: function() {
 		$(this.el).find('#noactivity').remove();
-		if (this.collection.length == 0) {
+		if (this.collection && this.collection.length == 0) {
 			$(this.el).append("<div id=noactivity class=well>No Record</div>")
 		}
 	}
@@ -72,21 +89,25 @@ App.CollectionView = Backbone.View.extend(
 		this.collection.on('all', this.handlChange, this);
 	},
 
-	handlChange: function(action, model) {
-		// console.log("handleChange: " + action);
+	handlChange: function(action, model) {		
+		console.log("collection change: " + action);
 		if (_(['sync']).contains(action)) {
 			this.synced = true;
 			this.render();
 		}
-		if (this.synced && _(['add']).contains(action)) this.renderModel(this.el, model);
-		if (this.synced && _(['remove']).contains(action)) this.removeModel(this.el, model);
+		if (!this.synced) return;
+		if (_(['add', 'change']).contains(action)) this.render();
+		if (_(['remove']).contains(action)) this.removeModel(this.el, model);
 	},
 
 	registerListeners: function(element) {
 		if (!this.options.listeners) return;
 		var self = this;
 		_(this.options.listeners).each(function(listener) {
-			listener.apply && listener.apply(element, self.collection);
+			listener.apply && listener.apply(element, self.collection, 
+				self.model || (self.collection && self.collection.where({ id: $(element).attr('data-id') })[0]),
+				self
+			);
 		})
 	}
 }));
@@ -95,83 +116,13 @@ App.ModelView = Backbone.View.extend(
 	_.extend( App.Mixins.HandlebarModelRenderer, {
 
 	initialize: function(options) {
+		this.options = options || {};
 		this.model.on('all', this.handlChange, this);
 	},
 
 	handlChange: function(action) {
-		// console.log("handleChange: " + action);
-		if (_(['add', 'change']).contains(action)) this.render();
+		console.log("model change: " + action);
+		if (_(['sync']).contains(action)) this.render();
 	}
 }));
-
-//Listeners
-App.Listener = Backbone.View.extend({
-	apply: function(element) {
-		//to be overriden
-	}
-});
-
-App.RemoveListener = App.Listener.extend({
-	apply: function(el, collection) {
-		$(el).find('.removeButton').on('click', function(e) {
-			e.stopPropagation();
-			if (!confirm('delete??')) return;
-			var model = collection.where({ id: $(el).attr('data-id') })[0];
-			model && collection.remove(model);
-		});
-	}
-});
-App.LinkListener = App.Listener.extend({
-	apply: function(el, collection) {
-		$(el).on('click', function(e) {
-			e.stopPropagation();
-			App.save($(el).attr('data-id-type') + '-id', $(el).attr('data-id'));
-			if ($(el).attr('data-target')) location.href=$(el).attr('data-target');
-		});
-	}
-});
-
-App.ContactSelectListener = App.Listener.extend({
-	apply: function(el, collection) {
-		$(el).find('.contactSelectButton').on('click', function(e) {
-			e.stopPropagation();
-			var id = $(e.target).parents('[data-id]').attr('data-id');
-
-			$('#activityContact').modal();
-
-			var contacts = new App.CollectionView({ 
-				collection: new App.PeopleCollection, 
-				el: $('#contactSelect'),
-				listeners: [ new App.YesOrNoListItemListener() ]  
-			});
-
-			contacts.on('rendered', function() {
-				var peopleList = collection.get(id).get('people');
-				_(peopleList).each(function(p, i) {
-					$(contacts.el).find('[data-id='+p.id+']')
-						.addClass('list-group-item-success')
-				})
-			})
-		});
-	}
-});
-
-
-App.YesOrNoListItemListener = App.Listener.extend({
-	apply: function(el, collection) {
-		$(el).find('.btn-success').on('click', function(e) {
-			e.stopPropagation();
-			$(e.target).parents('.list-group-item')
-				.removeClass('list-group-item-danger')
-				.addClass('list-group-item-success')
-		});
-		$(el).find('.btn-danger').on('click', function(e) {
-			e.stopPropagation();
-			$(e.target).parents('.list-group-item')
-				.removeClass('list-group-item-success')
-				.addClass('list-group-item-danger')
-		});
-	}
-});
-//Listeners end
 
--- silly.html/sillymodels.js
+++ silly.html/sillymodels.js
@@ -1,13 +1,11 @@
-App.Model = Backbone.Firebase.Model.extend({
-    initialize: function(options) {
-    	if (options.id) this.firebase = this.firebase + options.id
-    }
-});
+App.Model = Backbone.Firebase.Model.extend({ });
 App.Collection = Backbone.Firebase.Collection.extend({ });
 
-
-App.Activity = App.Model.extend({ firebase: "https://silly.firebaseio.com/activity/" });
+App.Activity = App.Model.extend({ firebase: "https://silly.firebaseio.com/activity/" + App.access('activity-id') });
 App.ActivityCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/activity" });
 
 App.People = App.Model.extend({ firebase: "https://silly.firebaseio.com/people/" });
 App.PeopleCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/people" });
+App.PeopleInActivityCollection = App.Collection.extend({ 
+	firebase: "https://silly.firebaseio.com/activity/" + App.access('activity-id') + "/people/",
+});
