5d5ec331ac685b0fbeabd78b4d2bccefa013bd20
Kinnnnn <kin.tang@gmail.com>
2014-07-25 00:25:25 +0800

    added group concept

--- silly.html/sillymodels.js
+++ silly.html/sillymodels.js
@@ -1,11 +1,14 @@
 App.Model = Backbone.Firebase.Model.extend({ });
 App.Collection = Backbone.Firebase.Collection.extend({ });
 
-App.Activity = App.Model.extend({ firebase: "https://silly.firebaseio.com/activity/" + App.access('activity-id') });
-App.ActivityCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/activity" });
+App.Activity = App.Model.extend({ firebase: "https://silly.firebaseio.com/" + App.access('group-id') + "/activity/" + App.access('activity-id') });
+App.ActivityCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/" + App.access('group-id') + "/activity" });
+
+App.Group = App.Model.extend({ firebase: "https://silly.firebaseio.com/group/" + App.access('group-id') });
+App.GroupCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/group" });
 
 App.People = App.Model.extend({ firebase: "https://silly.firebaseio.com/people/" });
 App.PeopleCollection = App.Collection.extend({ firebase: "https://silly.firebaseio.com/people" });
 App.PeopleInActivityCollection = App.Collection.extend({ 
-	firebase: "https://silly.firebaseio.com/activity/" + App.access('activity-id') + "/people/",
+	firebase: "https://silly.firebaseio.com/" + App.access('group-id') + "/activity/" + App.access('activity-id') + "/people/",
 });
